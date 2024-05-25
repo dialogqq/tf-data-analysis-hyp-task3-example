977758cd33cff4090d6aeaf1f7920ca469c6bfc1
dialogqq <dialogq956@gmail.com>
2024-05-25 19:19:51 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sps
 
+chat_id = 991556115 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(control_sample: pd.Series, test_sample: pd.Series) -> bool:
+    t_stat, p_value = stats.ttest_ind(control_sample, test_sample, equal_var=False)
+    ans = False
+    alpha = 0.06
+    if p_value < alpha:
+        ans = True
+    
+    return ans
