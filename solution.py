9f73aeb9915858c3f1b156cf3f2f4d5a84473c9a
dialogqq <dialogq956@gmail.com>
2024-05-25 19:20:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ import scipy.stats as sps
 chat_id = 991556115 # Ваш chat ID, не меняйте название переменной
 
 def solution(control_sample: pd.Series, test_sample: pd.Series) -> bool:
-    t_stat, p_value = stats.ttest_ind(control_sample, test_sample, equal_var=False)
+    t_stat, p_value = sps.ttest_ind(control_sample, test_sample, equal_var=False)
     ans = False
     alpha = 0.06
     if p_value < alpha:
